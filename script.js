62c1b59b7700eb76b530fa8319fff2e1628ece46
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-20 17:44:25 -0600

    created the skeleton for the form and the input and set up onReady functions

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('JS');
+
+$(document).ready(onReady);
+
+let employee = [];
+
+function onReady() {
+    console.log('READY!')
+    render();
+}
+
+function addEmployee(){
+
+    render();
+}
+
+
+function render() {
+    
+}
