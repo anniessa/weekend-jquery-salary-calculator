1fba90a6c6f254de1037d41a92c165f62372b5cf
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-20 19:56:45 -0600

    updated deleteEmployee function, having trouble getting delete button to show up in DOM

--- script.js
+++ script.js
@@ -2,19 +2,68 @@ console.log('JS');
 
 $(document).ready(onReady);
 
-let employee = [];
+let employees = [];
 
 function onReady() {
     console.log('READY!')
+    
+    $('#addEmployeeForm').on('submit', addEmployee);
+    $('#deleteEmployee').on('submit', deleteEmployee);
+
     render();
 }
 
-function addEmployee(){
+function addEmployee(evt){
+    console.log('in addEmployee', employees);
+    evt.preventDefault();
 
+    let newEmployee = {
+        firstName: $('#firstNameInput').val(),
+        lastName: $('#lastNameInput').val(),
+        id: Number($('#idInput').val()),
+        title: $('#titleInput').val(),
+        annualSalary: Number($('#annualSalaryInput').val()),
+    }
+
+    $('#firstNameInput').val(''), 
+    $('#lastNameInput').val(''),
+    Number($('#idInput').val('')),
+    $('#titleInput').val(''),
+    Number($('#annualSalaryInput').val('')),
+
+    employees.push(newEmployee);
+    render();
+}
+
+function deleteEmployee() {
+    console.log('in deleteEmployee');
+
+    let myTr = $(this).parent().parent();
+    let indexOfEmployee = myTr.index();
+    console.log('indexOfEmployee', indexOfEmployee);
+    cats.splice(indexOfEmployee, 1); 
     render();
 }
 
 
 function render() {
+    console.log('in render');
+    $('#employee-table').empty();
+
+    for (let employee of employees)
+    $('#employee-table').append(`
+    <tr>
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.id}</td>
+    <td>${employee.title}</td>
+    <td>${employee.annualSalary}</td>
+    <button class="deleteEmployee">
+    Delete
+    </button>
+    </tr>
+
+
+    `)
     
 }
