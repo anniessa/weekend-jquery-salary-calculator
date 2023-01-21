c28a9d2068818fd6711be13122387113b127cb0a
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-21 11:44:45 -0600

    updated the monthly cost function to subtract once employee is deleted

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ console.log('JS');
 $(document).ready(onReady);
 
 let employees = [];
+let totalMonthlyCosts = 0;
 
 function onReady() {
     console.log('READY!')
     
     $('#addEmployeeForm').on('submit', addEmployee);
+    $('#addEmployeeForm').on('submit', addMonthlyCost);
+
     $(document).on('click', '.delete-employee', deleteEmployee);
+    $(document).on('click', '.delete-employee', addMonthlyCost);
+    
 
     render();
 }
@@ -16,28 +21,38 @@ function onReady() {
 function addEmployee(evt){
     console.log('in addEmployee', employees);
     evt.preventDefault();
-
+    
     let newEmployee = {
-        firstName: $('#firstNameInput').val(),
-        lastName: $('#lastNameInput').val(),
+        firstName: $('#firstNameInput').val().toUpperCase(),
+        lastName: $('#lastNameInput').val().toUpperCase(),
         id: Number($('#idInput').val()),
-        title: $('#titleInput').val(),
+        title: $('#titleInput').val().toUpperCase(),
         annualSalary: Number($('#annualSalaryInput').val()),
     }
-
+    
     $('#firstNameInput').val(''), 
     $('#lastNameInput').val(''),
     Number($('#idInput').val('')),
     $('#titleInput').val(''),
     Number($('#annualSalaryInput').val('')),
-
+    
     employees.push(newEmployee);
     render();
 }
 
+function addMonthlyCost() {
+    totalMonthlyCosts = 0;
+    for (let employee of employees) {
+    totalMonthlyCosts += (employee.annualSalary / 12);
+    }
+    console.log('in monthly costs', totalMonthlyCosts);
+    render();
+}
+
+
 function deleteEmployee() {
     console.log('in deleteEmployee');
-
+    
     let myTr = $(this).parent().parent();
     let indexOfEmployee = myTr.index();
     console.log('indexOfEmployee', indexOfEmployee);
@@ -49,7 +64,7 @@ function deleteEmployee() {
 function render() {
     console.log('in render');
     $('#employee-table').empty();
-
+    
     for (let employee of employees)
     $('#employee-table').append(`
     <tr>
@@ -63,5 +78,14 @@ function render() {
     </td>
     </tr>
     `)
-    
+
+    console.log(totalMonthlyCosts);
+
+    $('#totalMonthly').text(`Monthly Cost $ ${totalMonthlyCosts}`);
+
+    if (totalMonthlyCosts >= 20000) {
+        $('#totalMonthly').addClass("red-background")
+    } else {
+        $('#totalMonthly').removeClass("red-background")
+    }
 }
