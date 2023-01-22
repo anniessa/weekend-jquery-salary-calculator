f17871baf1ab5db638a6ef3908c12b34afcb7ca1
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-22 11:47:57 -0600

    commented out my last bits of the edit button code, might try again after some guidance

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(document).ready(onReady);
 let employees = [];
 let totalMonthlyCosts = 0;
 
+
 function onReady() {
     console.log('READY!')
     
@@ -14,8 +15,8 @@ function onReady() {
     $(document).on('click', '.delete-btn', deleteEmployee);
     $(document).on('click', '.delete-btn', calculateMonthlyCost);
 
-    $(document).on('click', '.edit-btn', editEmployee);
-    $(document).on('click', '.end-editing', endEdit);
+    // $(document).on('click', '.edit-btn', editEmployee);
+    // $(document).on('click', '.end-editing', endEdit);
     
     
     render();
@@ -63,20 +64,17 @@ function deleteEmployee() {
     render();
 }
 
-function editEmployee() {
-    console.log('in edit employee button');
-    $(this).contentEditable = true;
-    $(this).parent().parent().addClass("edit-field");
-    
-   render();    
-}
+// function editEmployee() {
+//     console.log('in edit employee button');
+//     $("td").contentEditable = true;
+//    render();    
+// }
 
-function endEdit() {
-    console.log('in done edit button');
-    $(this).contentEditable = false;
-    $(this).parent().parent().removeClass("edit-field");
-    render();
-}
+// function endEdit() {
+//     console.log('in done edit button');
+//     $("td").contentEditable = false;
+//     render();
+// }
 
 
 function render() {
@@ -92,8 +90,8 @@ function render() {
     <td>${employee.title}</td>
     <td>${employee.annualSalary}</td>
     <td>
-    <button class="edit-btn">Edit</button>
-    <button class="end-editing">Done Editing</button>
+    <!-- <button class="edit-btn">Edit</button>
+    <button class="end-editing">Done Editing</button>-->
     <button class="delete-btn">Delete</button>
     </td>
     </tr>
@@ -109,4 +107,9 @@ function render() {
         $('#totalMonthly').removeClass("red-background")
     }    
 
+    // if ($("td").contentEditable = true) {
+    //     $("td").addClass("edit-field");
+    // } else {
+    //     $("td ").removeClass("edit-field");
+    // }
 }
